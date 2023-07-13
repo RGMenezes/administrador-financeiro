6daa7b60edee0a951597b363a412bd0d71920a4c
Rafael Menezes <faelgmp@gmail.com>
2023-07-12 23:47:31 -0300

    styles menu finalizado, começando funcionalidades do menu

--- financial-administrator/src/app/home/layout.js
+++ financial-administrator/src/app/home/layout.js
@@ -1,12 +1,17 @@
+"use client";
+import { useState } from "react";
+
 import styles from "./layout.module.css"
 
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 
 export default function layout({children}){
+    const [theme, setTheme] = useState(false);
+
     return(
-        <div className={styles.bg}>
-            <Header />
+        <div className={theme ? `${styles.bg}`: `${styles.bg} ${styles.dark}`}>
+            <Header setTheme={setTheme} />
 
             <main className={styles.main} >
                 {children}
--- financial-administrator/src/components/layout/Header.js
+++ financial-administrator/src/components/layout/Header.js
@@ -1,26 +1,110 @@
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
+import db from "@/api/db";
+
 import { HiMenu, HiLogout, HiSun, HiMoon } from "react-icons/hi";
 import {IoClose, IoSettingsSharp} from "react-icons/io5"
 import styles from "./Header.module.css";
 
+import Loading from "../system/Loading";
+import AlertBox from "../system/AlertBox";
+
 import Logo from "../../../public/logo/logo";
 import LinkText from "../system/LinkText";
 
-export default function Header(){
+export default function Header({setTheme}){
+
+    const router = useRouter();
+    const [user, setUser] = useState({});
+
+    const [menu, setMenu] = useState("slide_out");
+    const menuRef = useRef(null);
+    const menuIconRef = useRef(null);
+
+    const [loading, setLoading] = useState(false);
+    const [onAlert, setOnAlert] = useState({});
+
+    const [onTheme, setOnTheme] = useState(true);
+    const [fadeTheme, setFadeTheme] = useState("fade_out");
+    const [fadeThemeDark, setFadeThemeDark] = useState("fade_in");
+
+    useEffect(() => {
+        setLoading(true);
+        
+        db.get("/login/user").then(res => {
+            if(res.data.type === "object"){
+                setUser(res.data.data);
+                setOnTheme(res.data.data.theme);
+                if(res.data.data.theme){
+                    setFadeTheme("fade_out");
+                    setTimeout(() => {
+                        setOnTheme(false);
+                        setFadeThemeDark("fade_in");
+                    }, 300);
+                }else{
+                    setFadeThemeDark("fade_out");
+                    setTimeout(() => {
+                        setOnTheme(true);
+                        setFadeTheme("fade_in");
+                    }, 300);
+                };
+            }else{
+                setOnAlert(res.data);
+                router.push(res.data.redirect);
+            };
+        }).catch(err => {
+            console.log(`Erro ao conectar com o banco de dados: ${err}`);
+
+        }).finally(() => setLoading(false));
+    }, []);
+
+    useEffect(() => {
+        const handleOutsideScroll = () => setMenu("slide_out");
+        document.addEventListener('scroll', handleOutsideScroll);
+        return () => document.removeEventListener('scroll', handleOutsideScroll);
+    }, []);
+
+    useEffect(() => {
+        setTheme(onTheme);
+    }, [onTheme]);
+
+    const activeMenu = () => menu == "slide_in" ? setMenu("slide_out") : setMenu("slide_in");
+
+    function changeTheme(){
+        if(onTheme){
+            setFadeTheme("fade_out");
+            setTimeout(() => {
+                setOnTheme(false);
+                setFadeThemeDark("fade_in");
+            }, 300);
+        }else{
+            setFadeThemeDark("fade_out");
+            setTimeout(() => {
+                setOnTheme(true);
+                setFadeTheme("fade_in");
+            }, 300);
+        };
+    };
+
     return(
         <header className={styles.header_container} >
-            <div className={styles.logo}>
-                <Link href={"/home"}><Logo /></Link>
-            </div>
 
-            <div className={styles.menu_icon}>
+            {loading && <Loading />}
+            <AlertBox alert={onAlert} />
+
+            <Link className={styles.logo} href={"/home"}><Logo /></Link>
+
+            <div onClick={activeMenu} ref={menuIconRef} className={`${styles.menu_icon} ${styles.icon}`}>
                 <HiMenu />
             </div>
 
-            <section className={styles.menu} >
+            <section ref={menuRef} className={`${styles.menu} ${styles[menu]}`} >
                 <header>
-                    <IoClose className={styles.icon} />
-                    <h4>Oi Rafael!</h4>
+                    <IoClose onClick={activeMenu} className={styles.icon} />
+                    <h4>Oi {user.name}!</h4>
                 </header>
                 <nav>
                     <ul>
@@ -30,6 +114,7 @@ export default function Header(){
                         <li><LinkText to="/home/financial_goal" text="Visualizar metas" spaced={true} /></li>
                         <li><LinkText to="/home/data/edit" text="Atualizar dados" spaced={true} /></li>
                     </ul>
+                    <hr/>
                     <ul>
                         <li><LinkText to="/home/financial_report" text="Relatório financeiro" spaced={true} /></li>
                         <li><LinkText to="/home/guidance" text="Orientações" spaced={true} /></li>
@@ -38,8 +123,7 @@ export default function Header(){
                 <footer>
                     <IoSettingsSharp className={styles.icon} />
 
-                    <HiSun className={styles.icon} />
-                    <HiMoon className={styles.icon} />
+                    {onTheme ? <HiSun onClick={changeTheme} className={`${styles.icon} ${styles[fadeTheme]}`} /> : <HiMoon onClick={changeTheme} className={`${styles.icon} ${styles[fadeThemeDark]}`} />}
                     
                     <HiLogout className={styles.icon} />
                 </footer>
