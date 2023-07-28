6bcda45b98d806cc0228bc07ea022522937f5daf
Rafael Menezes <faelgmp@gmail.com>
2023-07-28 12:35:25 -0300

    register goal finalizado

--- financial-administrator/src/app/home/financial_goal/register/page.js
+++ financial-administrator/src/app/home/financial_goal/register/page.js
@@ -1,19 +1,19 @@
 'use client';
-import { useEffect, useState } from "react";
+import db from "@/api/db";
+import { useState } from "react";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 import styles from "./page.module.css";
 
-import AlertBox from "@/components/system/AlertBox";
 import Loading from "@/components/system/Loading";
 import LinkText from "@/components/system/LinkText";
 import InputNumber from "@/components/form/InputNumber";
 import InputTextArea from "@/components/form/InputTextArea";
 import Button from "@/components/form/Button";
+import AlertBox from "@/components/system/AlertBox";
 
 export default function FinancialGoalRegister() {
-
     const [onAlert, setOnAlert] = useState({});
-    const [onLoading, setOnLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const  [goal, setGoal] = useState(["goal_1"]);
 
@@ -22,18 +22,22 @@ export default function FinancialGoalRegister() {
 
     function submit(e){
         e.preventDefault();
+        setLoading(true);
 
-        console.log(e);
+        const arrayGoal = goal.map(item => [e.target[`Text_${item}`].value, parseInt(e.target[`Number_${item}`].value)]);
+
+        db.post("/register/financial_goal", arrayGoal).then((res) => {
+            setOnAlert(res.data);
+        }).catch(err => console.error(`Erro ao conectar ao banco de dados: ${err}`))
+        .finally(() => setLoading(false))
     };
-
-    // useEffect(() => console.log(goal), [goal]);
 
     return(
         <>
             <AlertBox alert={onAlert} />
-            {onLoading && <Loading />}
+            {loading && <Loading />}
 
-            <form className={styles.form}>
+            <form onSubmit={submit} className={styles.form}>
                 <h1>Criar metas</h1>
 
                 <section className={styles.amount_container}>
@@ -44,24 +48,26 @@ export default function FinancialGoalRegister() {
                     </div>
                 </section>
 
-                {goal.map((item) => 
-                    <div className={styles.data_container} key={item}>
-                        <InputTextArea
-                            text="Nome"
-                            id={`Text_${item}`}
-                            maxLength={100}
-                            minLength={1}
-                            rows={5}
-                        />
-                        <InputNumber 
-                            text="Valor: "
-                            placeholder="Valor da meta"
-                            id={`Number_${item}`}
-                            min={0}
-                            required={true}
-                        />
-                    </div>
-                )}
+                <section className={styles.goal_container}>
+                    {goal.map((item) =>
+                        <div key={item}>
+                            <InputTextArea
+                                text="Descreva a sua nova meta."
+                                id={`Text_${item}`}
+                                maxLength={100}
+                                minLength={1}
+                                rows={5}
+                            />
+                            <InputNumber
+                                text="Valor: "
+                                placeholder="Valor da meta"
+                                id={`Number_${item}`}
+                                min={0}
+                                required={true}
+                            />
+                        </div>
+                    )}
+                </section>
 
                 <div className={styles.container_x}>
                     <LinkText 
