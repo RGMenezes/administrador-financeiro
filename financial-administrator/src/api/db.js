32f7567d0e1fa2d876a3fcae9273bb57ecf91d26
Rafael Menezes <faelgmp@gmail.com>
2023-08-11 13:17:22 -0300

    adicionando api online

--- financial-administrator/src/api/db.js
+++ financial-administrator/src/api/db.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiLocal = "http://localhost:8081/";
-const apiURL = "";
+const apiURL = "https://api-administrador-financeiro.vercel.app/";
 
 export default axios.create({
     baseURL: apiURL || apiLocal
