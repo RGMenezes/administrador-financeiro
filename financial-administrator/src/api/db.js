27d3ca63e27a01174970f4417ef09c04d67ce527
Rafael Menezes <faelgmp@gmail.com>
2023-08-13 13:34:50 -0300

    alterando logica de login

--- financial-administrator/src/api/db.js
+++ financial-administrator/src/api/db.js
@@ -4,6 +4,6 @@ const apiLocal = "http://localhost:8081/";
 const apiURL = "https://api-administrador-financeiro.vercel.app/";
 
 export default axios.create({
-    baseURL: apiURL || apiLocal,
+    baseURL: apiLocal,
     withCredentials: true
 });
