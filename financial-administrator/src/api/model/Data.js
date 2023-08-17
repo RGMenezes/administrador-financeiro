a2a20e9b40e9917864797cc53b753f37dd292366
Rafael Menezes <faelgmp@gmail.com>
2023-08-16 23:34:44 -0300

    retirando libs, aprimorando models mongo

--- financial-administrator/src/api/model/Data.js
+++ financial-administrator/src/api/model/Data.js
@@ -1,26 +1,35 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const Data = new Schema({
-    investment: {
-        type: Array,
-        default: []
-    },
-    expense: {
-        type: Array,
-        default: []
-    },
-    financialGoal: {
-        type: Array,
-        default: []
-    },
-    financialReport: {
-        type: Object
-    },
-    id:{
-        type: String,
-        require: true
-    }
-});
+let Data;
 
-export default mongoose.model("data", Data);
+try{
+    Data = mongoose.model("data");
+}catch (error){
+    const DataSchema = new Schema({
+        investment: {
+            type: Array,
+            default: []
+        },
+        expense: {
+            type: Array,
+            default: []
+        },
+        financialGoal: {
+            type: Array,
+            default: []
+        },
+        financialReport: {
+            type: Object
+        },
+        id:{
+            type: String,
+            require: true
+        }
+    });
+
+    Data = mongoose.model("data", DataSchema);
+};
+
+
+export default Data;
--- financial-administrator/src/api/model/User.js
+++ financial-administrator/src/api/model/User.js
@@ -1,27 +1,35 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const User = new Schema({
-    userName: {
-        type: String,
-        require: true
-    },
-    email: {
-        type: String,
-        require: true
-    },
-    password: {
-        type: String,
-        require: true
-    },
-    wage: {
-        type: Number, 
-        require: true
-    },
-    theme: {
-        type: Boolean,
-        default: false
-    }
-});
+let User;
 
-export default mongoose.model("users", User);
+try {
+    User = mongoose.model("users");
+} catch (error) {
+    const UserSchema = new Schema({
+        userName: {
+            type: String,
+            require: true
+        },
+        email: {
+            type: String,
+            require: true
+        },
+        password: {
+            type: String,
+            require: true
+        },
+        wage: {
+            type: Number, 
+            require: true
+        },
+        theme: {
+            type: Boolean,
+            default: false
+        }
+    });
+
+    User = mongoose.model("users", UserSchema);
+};
+
+export default User;
